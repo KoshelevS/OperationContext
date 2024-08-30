a4c73d3c62aa943c8082bfe0a53cb46b2f39ac79
Sergey Koshelev <Sergey.Koshelev@dataart.com>
2024-08-30 16:27:26 +0200

    test: add unit tests for the OperationContext.Core project

---
+++ OperationContext.Core.Tests/DefaultOperationContextTests.cs
@@ -0,0 +1,60 @@
+﻿namespace OperationContext.Core.Tests;
+
+public class DefaultOperationContextTests
+{
+    [Test]
+    public void StoresValues()
+    {
+        var target = new DefaultOperationContext { Items = new Dictionary<string, string>() };
+
+        target.Items.Add("Test", "Passed");
+        target.Items.Add("AnotherTest", "Passed again");
+
+        Assert.That(target.Items, Contains.Key("Test").WithValue("Passed"));
+        Assert.That(target.Items, Contains.Key("AnotherTest").WithValue("Passed again"));
+        Assert.That(target.Items, Has.Exactly(2).Items);
+    }
+
+    [Test]
+    public Task UsesSeparateStorageForEachAsyncOperation()
+    {
+        var target = new DefaultOperationContext();
+
+        var task1 = Task.Run(() =>
+        {
+            target.Items = new Dictionary<string, string>();
+            target.Items.Add("Test", "Passed");
+
+            Assert.That(target.Items, Contains.Key("Test").WithValue("Passed"));
+            Assert.That(target.Items, Has.One.Items);
+        });
+        var task2 = Task.Run(() =>
+        {
+            target.Items = new Dictionary<string, string>();
+            target.Items.Add("AnotherTest", "Passed again");
+
+            Assert.That(target.Items, Contains.Key("AnotherTest").WithValue("Passed again"));
+            Assert.That(target.Items, Has.One.Items);
+        });
+        var task3 = Task.Delay(100);
+
+        return Task.WhenAll(task1, task2, task3);
+    }
+
+    [Test]
+    public async Task CleansOperationContextUponCompletion()
+    {
+        var target = new DefaultOperationContext();
+
+        var task1 = Task.Run(() =>
+        {
+            target.Items = new Dictionary<string, string>();
+            target.Items.Add("Test", "Passed");
+        });
+        var task2 = Task.Delay(100);
+
+        await Task.WhenAll(task1, task2);
+
+        Assert.That(target.Items, Is.Null);
+    }
+}
---
+++ OperationContext.Core.Tests/OperationContextLogScopeTests.cs
@@ -0,0 +1,62 @@
+﻿namespace OperationContext.Core.Tests;
+
+public class OperationContextLogScopeTests
+{
+    [Test]
+    public void WrapsProvidedDictionary()
+    {
+        var source = new Dictionary<string, string>()
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" },
+        };
+        var target = new OperationContextLogScope(source);
+
+        Assert.That(target, Has.Exactly(source.Count).Items);
+    }
+
+    [Test]
+    public void Index_ReturnsItemsFromSourceDictionary()
+    {
+        var source = new Dictionary<string, string>()
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" },
+        };
+        var target = new OperationContextLogScope(source);
+
+        Assert.That(target[0], Is.EqualTo(KeyValuePair.Create("Key1", (object)"Value1")));
+        Assert.That(target[1], Is.EqualTo(KeyValuePair.Create("Key2", (object)"Value2")));
+    }
+
+    [Test]
+    public void GetEnumerator_ReturnsItemsFromSourceDictionary()
+    {
+        var source = new Dictionary<string, string>()
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" },
+        };
+        var target = new OperationContextLogScope(source);
+        var enumerator = target.GetEnumerator();
+
+        enumerator.MoveNext();
+        Assert.That(enumerator.Current, Is.EqualTo(KeyValuePair.Create("Key1", (object)"Value1")));
+
+        enumerator.MoveNext();
+        Assert.That(enumerator.Current, Is.EqualTo(KeyValuePair.Create("Key2", (object)"Value2")));
+    }
+
+    [Test]
+    public void ToString_SerializesItemsToString()
+    {
+        var source = new Dictionary<string, string>()
+        {
+            { "Key1", "Value1" },
+            { "Key2", "Value2" },
+        };
+        var target = new OperationContextLogScope(source);
+
+        Assert.That(target.ToString(), Is.EqualTo("Key1: Value1, Key2: Value2"));
+    }
+}
